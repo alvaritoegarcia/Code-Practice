36623c03006389133bf2a2369c470aabae515e58
alvaritoegarcia <alvaroenrique.gar2005@gmail.com>
2024-03-11 23:46:12 -0300

    first commit

---
+++ Linear Search.cs
@@ -0,0 +1,47 @@
+﻿using System;
+namespace Practices
+{
+	public class Linear_Search
+	{
+		public Linear_Search()
+		{
+            int i, flag;
+            int NumberOfElements;
+            double RequestedElement;
+
+
+            Console.WriteLine("Digite el número de elementos a ser introducidos");
+            NumberOfElements = Convert.ToInt32(Console.ReadLine());
+
+            double[] Elements = new double[NumberOfElements];
+
+            for (i = 0; i < NumberOfElements; i++)
+            {
+                Console.WriteLine("Digite los elementos a añadir");
+                Elements[i] = Convert.ToDouble(Console.ReadLine());
+
+            }
+
+
+            Console.WriteLine("Digite el elemento a ser buscado");
+            RequestedElement = Convert.ToDouble(Console.ReadLine());
+
+            flag = 0;
+
+            for (i = 0; i < NumberOfElements; i++)
+            {
+
+                if (RequestedElement == Elements[i])
+                {
+                    Console.WriteLine("El elemento " + RequestedElement + " ha sido hallado en la posición: " + i++);
+                    flag = 1;
+                }
+
+            }
+
+            if (flag == 0)
+                Console.WriteLine("El elemento no se ha encontrado");
+        }
+	}
+}
+
---
+++ Stacks.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace Practices
+{
+	public class Stacks1
+	{
+		public Stacks1()
+		{
+            // Stacks Data Structures
+
+
+
+            Stack<string> stack1 = new Stack<string>();
+
+            //Pushing
+
+            stack1.Push("Jhon");
+            stack1.Push("Kate");
+            stack1.Push("Edward");
+            stack1.Push("Roxy");
+
+
+            // Iterating and popping
+
+            while (stack1.Count != 0)
+            {
+
+                Console.WriteLine(stack1.Pop());
+            }
+
+
+        }
+    }
+}
+
